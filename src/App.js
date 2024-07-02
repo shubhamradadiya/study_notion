0050ea6b99092d7c852edb20be0bf1a97363b6cb
shubhamradadiya <shubhamradadiya98@gmail.com>
2024-07-02 17:55:51 +0530

    Redux_Setup

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Navbar from './components/common/Navbar';
 
 function App() {
   return (
     <div className=' w-screen min-h-screen flex flex-col font-inter bg-richblack-900'>
+       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
       </Routes>
--- src/index.js
+++ src/index.js
@@ -4,13 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducers from './reducer';
+import { Provider } from 'react-redux';
+
+const store = configureStore({
+  reducer:rootReducers
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-     <App />
-    </BrowserRouter>
+     <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+     </Provider>
   </React.StrictMode>
 );
 
