4428aba5846fcef46209c9401949437550735255
shubhamradadiya <shubhamradadiya98@gmail.com>
2024-07-03 20:59:32 +0530

    api  changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './components/common/Navbar';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
@@ -10,7 +12,10 @@ function App() {
        <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
+        <Route path='login' element={<Login/>}/>
+        <Route path='signup'element ={<Signup/>}/>
       </Routes>
+    
     </div>
   );
 }
--- src/components/core/Auth/LoginForm.jsx
+++ src/components/core/Auth/LoginForm.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
 const LoginForm = () => {
-    
+
   return (
     <div>
-
-
+        <div>
+            <div></div>
+            <div>
+                <form>
+                  <lable>
+                    
+                  </lable>
+                </form>
+            </div>
+        </div>
     </div>
   )
 }
