c1d221f61df2177425a2cd5cb42382888b4f448a
shubhamradadiya <shubhamradadiya98@gmail.com>
2024-07-09 21:19:33 +0530

    FX: sidebar

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Outlet } from 'react-router-dom'
+import Sidebar from '../components/core/Dashboard/Sidebar'
 
 const Dashboard = () => {
 
@@ -8,17 +9,20 @@ const Dashboard = () => {
    const {loading : profileLoading } = useSelector ((state)=> state.profile)
 
    if(authLoading || profileLoading){
-    return <div className='spinner'></div>
+    return <div className=' grid place-items-center min-h-[calc(100vh-3.5rem)] '>
+      <div className='spinner'></div>
+    </div>
    }
 
   return (
     <div>
-        <div>
-
-            
-        <div>
-            <Outlet/>
-        </div>
+        <div className=' relative min-h-[calc(100vh-3.5rem)]'>
+            <Sidebar/>
+            <div className=' min-h-[calc(100vh-3.5rem)] flex-1 overflow-auto'>
+                <div className=' mx-auto w-11/12  max-w-[1000px] py-10'>
+                  <Outlet/>
+                </div>
+            </div>
         </div>
     </div>
   )
