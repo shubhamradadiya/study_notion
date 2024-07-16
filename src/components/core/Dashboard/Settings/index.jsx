3ee21dad04236fd115a6793c19d9f32bef5944a8
shubhamradadiya <shubhamradadiya98@gmail.com>
2024-07-16 22:43:26 +0530

    ADD:EditProfileSection

--- src/components/core/Dashboard/Settings/index.jsx
+++ src/components/core/Dashboard/Settings/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ChangeProfilePicture from './ChangeProfilePicture'
+import EditProfile from './EditProfile'
 
 const Settings = () => {
   return (
     <>
-        <h1 className='mb-14 text-3xl font-medium text-richblack-5'> Edit Profile</h1>
+        <h1 className='mb-14 text-3xl font-medium text-richblack-5 '> Edit Profile</h1>
 
         {/* profile picture change */}
         <ChangeProfilePicture/>
+        
+         {/* profile details  */}
+        <EditProfile/>
     </>
   )
 }
--- src/services/operations/SettingsAPI.js
+++ src/services/operations/SettingsAPI.js
@@ -7,7 +7,7 @@ import { setUser } from "../../slices/profileSlice";
 
 const {
     UPDATE_DISPLAY_PICTURE_API,
-    // UPDATE_PROFILE_API,
+    UPDATE_PROFILE_API,
     // CHANGE_PASSWORD_API,
     // DELETE_PROFILE_API
 } =settingsEndpoints
@@ -38,6 +38,38 @@ export function updateDisplayPicture(token, formData) {
     } catch (error) {
       console.log("UPDATE_DISPLAY_PICTURE_API API ERROR............", error)
       toast.error("Could Not Update Display Picture")
+    }
+    toast.dismiss(toastId)
+  }
+}
+
+export function updateProfile(token , formData){
+  return async (dispatch)=>{
+    const  toastId = toast.loading("Loading...");
+    try {
+      const response = await apiConnector("PUT",UPDATE_PROFILE_API, formData,{
+        Authorisation: `Bearer ${token}`,
+      })
+
+      console.log("UPDATE_PROFILE_API API RESPONSE............", response)
+
+      if (!response.data.success) {
+        throw new Error(response.data.message)
+      }
+
+      const userImage = response.data.updatedUserDetails.image
+      ? response.data.updatedUserDetails.image
+      : `https://api.dicebear.com/5.x/initials/svg?seed=${response.data.updatedUserDetails.firstName} ${response.data.updatedUserDetails.lastName}`
+   
+      dispatch(
+      setUser({ ...response.data.updatedUserDetails, image: userImage })
+    )
+
+    toast.success("Profile Updated Successfully")
+
+    } catch (error) {
+      console.log("UPDATE_PROFILE_API API ERROR............", error)
+      toast.error("Could Not Update Profile")
     }
     toast.dismiss(toastId)
   }
