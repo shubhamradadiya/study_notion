46d4d9eda924fe47f8699216203db2170f665f84
shubhamradadiya <shubhamradadiya98@gmail.com>
2024-07-19 00:04:38 +0530

    ADD:GetCourseAPI

---
+++ src/services/operations/courseDetailsAPI.js
@@ -0,0 +1,68 @@
+import toast from "react-hot-toast";
+import { apiConnector } from "../apiconnector";
+import { courseEndpoints } from "../apis"
+import { UNSAFE_ErrorResponseImpl } from "react-router-dom";
+
+const {
+    COURSE_DETAILS_API,
+    COURSE_CATEGORIES_API,
+    GET_ALL_COURSE_API,
+    CREATE_COURSE_API,
+    EDIT_COURSE_API,
+    CREATE_SECTION_API,
+    CREATE_SUBSECTION_API,
+    UPDATE_SECTION_API,
+    UPDATE_SUBSECTION_API,
+    DELETE_SECTION_API,
+    DELETE_SUBSECTION_API,
+    GET_ALL_INSTRUCTOR_COURSES_API,
+    DELETE_COURSE_API,
+    GET_FULL_COURSE_DETAILS_AUTHENTICATED,
+    CREATE_RATING_API,
+    LECTURE_COMPLETION_API,
+} = courseEndpoints
+
+
+export const getAllCourses = async () =>{
+  
+    const  toastId =  toast.loading("Loading...");
+    let result = [];
+
+    try {
+        const  response = await apiConnector("GET",GET_ALL_COURSE_API);
+        if(response?.data?.success){
+         throw new Error(response.data.message)
+        } 
+        result = response?.data?.data
+
+    } catch (error) {
+        console.log("GET_ALL_COURSE_API API ERROR............", error)
+        toast.error(error.message)
+    }
+    toast.dismiss(toastId)
+    return result;
+}
+
+export const fetchCourseDetails = async (courseId) => {
+    const toastId = toast.loading("Loading...")
+    //   dispatch(setLoading(true));
+    let result = null
+    try {
+      const response = await apiConnector("POST", COURSE_DETAILS_API, {
+        courseId,
+      })
+      console.log("COURSE_DETAILS_API API RESPONSE............", response)
+  
+      if (!response.data.success) {
+        throw new Error(response.data.message)
+      }
+      result = response.data
+    } catch (error) {
+      console.log("COURSE_DETAILS_API API ERROR............", error)
+      result = error.response.data
+      // toast.error(error.response.data.message);
+    }
+    toast.dismiss(toastId)
+    //   dispatch(setLoading(false));
+    return result
+  }
