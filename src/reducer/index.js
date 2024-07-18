b428b55cf0ad01838e75dddf2dd75a73237924ac
shubhamradadiya <shubhamradadiya98@gmail.com>
2024-07-18 23:17:17 +0530

    ADD:CourseSlice

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from  "../slices/authSlice"
 import profileReducer from "../slices/profileSlice";
 import cartReducer from "../slices/cartSlice"
+import courseSlice from "../slices/courseSlice";
 
 const rootReducers = combineReducers({
    auth  : authReducer,
    profile:profileReducer,
    cart:cartReducer,
+   course:courseSlice
 })
 
 export default rootReducers
---
+++ src/slices/courseSlice.js
@@ -0,0 +1,39 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    step: 1,
+    course: null,
+    editCourse: false,
+    paymentLoading: false,
+}
+
+const courseSlice = createSlice(
+    {
+        name : "Course",
+        initialState,
+        reducers:{
+            setStep:(state, action) =>{
+                state.step= action.payload;
+            },
+            setCourse:(state, action) =>{
+                state.course= action.payload;
+            },
+            setEditCourse:(state, action) =>{
+                state.editCourse= action.payload;
+            },
+            setPaymentLoading:(state, action) =>{
+                state.paymentLoading= action.payload;
+            },
+            resetCourseState:(state, action) =>{
+                state.course=null;
+                state.step=1;
+                state.editCourse = false;
+            }    
+        }
+    }
+)
+
+     
+export const  {setStep ,setCourse , setEditCourse , setPaymentLoading, resetCourseState} = courseSlice.actions
+
+export default courseSlice.reducer
