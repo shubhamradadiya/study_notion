a881ed5a97f948dbea6decccf22234bc5f84d29d
shubhamradadiya <shubhamradadiya98@gmail.com>
2024-07-22 11:33:08 +0530

    ADD: Step 1  While Edit ModeON

--- src/components/core/Dashboard/AddCourse/CourseInformation/CourseInformationForm.jsx
+++ src/components/core/Dashboard/AddCourse/CourseInformation/CourseInformationForm.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { HiOutlineCurrencyRupee } from 'react-icons/hi'
-import { addCourseDetails, fetchCourseCategories } from '../../../../../services/operations/courseDetailsAPI'
+import { addCourseDetails, editCourseDetails, fetchCourseCategories } from '../../../../../services/operations/courseDetailsAPI'
 import ChipInput from './ChipInput'
 import Upload from './Upload'
 import { useDispatch, useSelector } from 'react-redux'
@@ -10,6 +10,7 @@ import { setCourse, setStep } from '../../../../../slices/courseSlice'
 import { MdNavigateNext } from 'react-icons/md'
 import IconBtn from '../../../../common/IconBtn'
 import { COURSE_STATUS } from '../../../../../utils/constants'
+import toast from 'react-hot-toast'
 
 const CourseInformationForm = () => {
 
@@ -40,11 +41,97 @@ const CourseInformationForm = () => {
       }
     }
 
+    // if Edit Mode On 
+    if (editCourse) {
+      // console.log("data populated", editCourse)
+      setValue("courseTitle", course.courseName)
+      setValue("courseShortDesc", course.courseDescription)
+      setValue("coursePrice", course.price)
+      setValue("courseTags", course.tag)
+      setValue("courseBenefits", course.whatYouWillLearn)
+      setValue("courseCategory", course.category)
+      setValue("courseRequirements", course.instructions)
+      setValue("courseImage", course.thumbnail)
+    }
+
     getCategory();
   },[])
 
+  const isFormUpdated = () => {
+    const currentValues = getValues()
+    // console.log("changes after editing form values:", currentValues)
+    if (
+      currentValues.courseTitle !== course.courseName ||
+      currentValues.courseShortDesc !== course.courseDescription ||
+      currentValues.coursePrice !== course.price ||
+      currentValues.courseTags.toString() !== course.tag.toString() ||
+      currentValues.courseBenefits !== course.whatYouWillLearn ||
+      currentValues.courseCategory._id !== course.category._id ||
+      currentValues.courseRequirements.toString() !==
+        course.instructions.toString() ||
+      currentValues.courseImage !== course.thumbnail
+    ) {
+      return true
+    }
+    return false
+  }
+
   // Submit data
   const onSubmit = async (data) =>{
+    if (editCourse) {
+      // const currentValues = getValues()
+      // console.log("changes after editing form values:", currentValues)
+      // console.log("now course:", course)
+      // console.log("Has Form Changed:", isFormUpdated())
+      if (isFormUpdated()) {
+        const currentValues = getValues()
+        const formData = new FormData()
+        // console.log(data)
+        formData.append("courseId", course._id)
+        if (currentValues.courseTitle !== course.courseName) {
+          formData.append("courseName", data.courseTitle)
+        }
+        if (currentValues.courseShortDesc !== course.courseDescription) {
+          formData.append("courseDescription", data.courseShortDesc)
+        }
+        if (currentValues.coursePrice !== course.price) {
+          formData.append("price", data.coursePrice)
+        }
+        if (currentValues.courseTags.toString() !== course.tag.toString()) {
+          formData.append("tag", JSON.stringify(data.courseTags))
+        }
+        if (currentValues.courseBenefits !== course.whatYouWillLearn) {
+          formData.append("whatYouWillLearn", data.courseBenefits)
+        }
+        if (currentValues.courseCategory._id !== course.category._id) {
+          formData.append("category", data.courseCategory)
+        }
+        if (
+          currentValues.courseRequirements.toString() !==
+          course.instructions.toString()
+        ) {
+          formData.append(
+            "instructions",
+            JSON.stringify(data.courseRequirements)
+          )
+        }
+        if (currentValues.courseImage !== course.thumbnail) {
+          formData.append("thumbnailImage", data.courseImage)
+        }
+        // console.log("Edit Form data: ", formData)
+        setLoading(true)
+        const result = await editCourseDetails(formData, token)
+        setLoading(false)
+        if (result) {
+          dispatch(setStep(2))
+          dispatch(setCourse(result))
+        }
+      } else {
+        toast.error("No changes made to the form")
+      }
+      return
+    }
+
     console.log("FORM  DATA.........................")
     console.log(data)
 
--- src/services/operations/courseDetailsAPI.js
+++ src/services/operations/courseDetailsAPI.js
@@ -105,3 +105,24 @@ export const getAllCourses = async () =>{
     return result
   }
   
+  export const editCourseDetails = async (data, token) => {
+    let result = null
+    const toastId = toast.loading("Loading...")
+    try {
+      const response = await apiConnector("POST", EDIT_COURSE_API, data, {
+        "Content-Type": "multipart/form-data",
+        Authorisation: `Bearer ${token}`,
+      })
+      console.log("EDIT COURSE API RESPONSE............", response)
+      if (!response?.data?.success) {
+        throw new Error("Could Not Update Course Details")
+      }
+      toast.success("Course Details Updated Successfully")
+      result = response?.data?.data
+    } catch (error) {
+      console.log("EDIT COURSE API ERROR............", error)
+      toast.error(error.message)
+    }
+    toast.dismiss(toastId)
+    return result
+  }
